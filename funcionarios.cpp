2b3dccd3f80db08ad19c34ee6b3c632cd31dbb80
MateusYudi <mateus_yudi@hotmail.com>
2016-09-26 10:47:48 -0300

    Update funcionarios.cpp

--- funcionarios.cpp
+++ funcionarios.cpp
@@ -2,4 +2,29 @@
 #include <string>
 using namespace std;
 struct funcionario;
-int registring
+int registro;
+string nome;
+double salario;
+(;
+void ler_funcionario(Funcionario*);
+void imprimir_funcionario(Funcionario);
+int main (){
+funcionario f;
+ler_funcionario(%f)}
+imprimir_funcionario(f);
+)
+void ler_funcionario(Funcionario* f){
+cout << "Registros: ";
+cin >> f->registro;
+cin.ignore();
+cout << "Nome: ";
+getline (cin, f->nome);
+cout << "Salario: ";
+cin >> f-> salario;
+
+
+
+
+
+
+}
