9509b47a007b26120ded7a871a82f7f99832d780
MateusYudi <mateus_yudi@hotmail.com>
2016-09-26 11:21:52 -0300

    Update funcionarios.cpp

--- funcionarios.cpp
+++ funcionarios.cpp
@@ -1,30 +1,67 @@
+
 #include <iostream>
 #include <string>
 using namespace std;
-struct funcionario;
-int registro;
-string nome;
-double salario;
-(;
+
+struct Funcionario{
+    int registro;
+    string nome;
+    double salario;
+
+    void ler_dados(){
+        cout << "Registro: ";
+        cin >> registro;
+        cin.ignore();
+        cout << "Nome: ";
+        getline(cin, nome);
+        cout << "Salário: ";
+        cin >> salario;
+    }
+};
+
+Funcionario criar_funcionario();
 void ler_funcionario(Funcionario*);
 void imprimir_funcionario(Funcionario);
-int main (){
-funcionario f;
-ler_funcionario(%f)}
-imprimir_funcionario(f);
-)
+
+int main(){
+    Funcionario f1;
+    ler_funcionario(&f1);
+    imprimir_funcionario(f1);
+
+    Funcionario f2 = criar_funcionario();
+    imprimir_funcionario(f2);
+
+    Funcionario f3;
+    f3.ler_dados();
+    imprimir_funcionario(f3);
+
+    //imprimir_funcionario(criar_funcionario());
+}
+
+Funcionario criar_funcionario(){
+    Funcionario f;
+    cout << "Registro: ";
+    cin >> f.registro;
+    cin.ignore();
+    cout << "Nome: ";
+    getline(cin, f.nome);
+    cout << "Salário: ";
+    cin >> f.salario;
+    return f;
+}
+
 void ler_funcionario(Funcionario* f){
-cout << "Registros: ";
-cin >> f->registro;
-cin.ignore();
-cout << "Nome: ";
-getline (cin, f->nome);
-cout << "Salario: ";
-cin >> f-> salario;
+    cout << "Registro: ";
+    cin >> f->registro;
+    cin.ignore();
+    cout << "Nome: ";
+    getline(cin, f->nome);
+    cout << "Salário: ";
+    cin >> f->salario;
+}
 
-
-
-
-
-
+void imprimir_funcionario(Funcionario f){
+    cout << "Registro: " << f.registro << endl;
+    cout << "Nome: " << f.nome << endl;
+    cout << "Salário: " << f.salario << endl;
 }
